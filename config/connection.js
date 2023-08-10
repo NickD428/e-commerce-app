4a43151d827f4881a633ead6f3e0e4c457c1efbf
Nick <chargerfan428@aol.com>
2023-08-09 19:09:28 -0700

    updated config and serverjs for future jawsdb and port listening

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+console.log (process.env.DB_NAME,process.env.DB_USER)
+require('dotenv').config();
+let sequelize;
+console.log (process.env.DB_NAME, process.env.DB_USER)
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
     });
-
+} else {
+    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, null, {
+        host: '127.0.0.1',
+        dialect: 'mysql',
+        port: 3306
+    });
+}
 module.exports = sequelize;
