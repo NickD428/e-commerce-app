0168c2309fdbcf2c3b212f8113731b0e08ac7118
Nick <chargerfan428@aol.com>
2023-08-12 10:47:30 -0700

    fixed connections and models to fit routes

--- models/index.js
+++ models/index.js
@@ -6,28 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE',
-});
-
+  foreignKey: 'category_id'
+})
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id',
-});
+  foreignKey: 'category_id'
+})
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {
+Product.belongsToMany(Tag,{
   through: ProductTag,
-  // as: 'product_tags',
-  foreignKey: 'product_id',
-});
+  foreignKey: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {
+Tag.belongsToMany( Product, {
   through: ProductTag,
-  // as: 'product_tags',
-  foreignKey: 'tag_id',
-});
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
